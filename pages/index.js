e6039b8fd39b37211849bfd3b5d2c286f32321c6
Monica Powell <monica@aboutmonica.com>
2020-03-21 15:35:19 -0400

    update styling of grid and colors

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import fetch from "isomorphic-unfetch";
+import colors from "./styles/colors";
 
 const Index = ({ websites }) => (
-  <>
-    <h1>Website Gallery</h1>
+  <div
+    style={{
+      textAlign: `center`,
+      color: colors.primaryHeadline,
+      fontSize: `16px`
+    }}
+  >
+    <h1>Monica*s Inspiration Gallery</h1>
+    <h2 style={{ color: colors.accent }}>
+      sites around the world wide web 🌏 that have inspired me.
+    </h2>
     <div id="flexContainer">
       {websites.length > 0 &&
         websites.map((url, i) => (
@@ -12,27 +22,38 @@ const Index = ({ websites }) => (
               {url.name}
             </div>
             <div key={i}>
-              <a href={url.address}>{url.address}</a>
+              <a href={url.address}>
+                {url.address}
+                <img src={url.photo} alt={url.name} />
+              </a>
             </div>
-            <img src={url.photo} alt={url.name} />
           </div>
         ))}
       <style jsx>{`
         #flexContainer {
           display: flex;
           flex-wrap: wrap;
+          justify-content: space-around;
         }
         .title {
           font-weight: bold;
+          font-size: 1.5em;
+          color: ${colors.secondaryHeadline};
         }
         .website {
-          width: 50%;
+          width: 30%;
+          padding: 1em;
+          margin: 20px;
+        }
+        .website:nth-child(odd),
+        .website:nth-child(even) {
+          background-color: ${colors.lightBackground};
         }
         ,
         img {
           width: 100%;
         }
-        ,
+
         h1,
         a {
           font-family: "Arial";
@@ -40,15 +61,30 @@ const Index = ({ websites }) => (
 
         a {
           text-decoration: none;
-          color: blue;
+          color: ${colors.accent};
+          font-size: 1.2em;
         }
 
         a:hover {
           opacity: 0.6;
         }
+        @media (max-width: 992px) {
+          .website {
+            width: 40%;
+          }
+        }
+        @media (max-width: 768px) {
+          #flexContainer {
+            flex-direction: column;
+            justify-content: center;
+          }
+          .website {
+            width: 90%;
+          }
+        }
       `}</style>
     </div>
-  </>
+  </div>
 );
 
 export async function getServerSideProps({ req }) {
